dc1285b2a3b18f6be95b0deb09c154af5c0c0f62
korbut13 <102421027+korbut13@users.noreply.github.com>
2023-07-04 01:08:49 +0200

    fix: clear dataBase

--- src/dataBase.ts
+++ src/dataBase.ts
@@ -1,9 +1,3 @@
 import { User } from "./utils/types";
 
 export const dataBase = new Map<string, User>();
-
-dataBase.set('7c2847e1c32fa29fc1bbca873bd787fb', {
-  username: 'Sveta',
-  age: 30,
-  hobbies: ['photo', 'reading']
-});
